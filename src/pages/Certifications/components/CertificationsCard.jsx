bd8f2242d60eef2f3f685c487ebf731840f16aba
Juanco <juanffo874@gmail.com>
2026-03-03 22:09:41 -0600

    styles: Agrega estilos al componente de certificaciones

--- src/pages/Certifications/components/CertificationsCard.jsx
+++ src/pages/Certifications/components/CertificationsCard.jsx
@@ -7,10 +7,10 @@ export default function CertificationsCard() {
             <div className="space-y-12">
                 <div className="text-center space-y-4">
                     <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                        Proyectos Destacados
+                        Certificaciones Destacadas
                     </h2>
                     <p className="mx-auto max-w-[700px] text-slate-500">
-                        Una selección de mis trabajos más recientes y proyectos personales
+                       Mis certificaciones más importantes y reconocidas en la industria
                     </p>
                 </div>
                 <div className="grid gap-8 mx-8 md:grid-cols-2">
@@ -18,21 +18,26 @@ export default function CertificationsCard() {
                         <div key={index} className="p-8 border border-gray-300 rounded-xl">
                             <div className="">
                                 <h2 className="font-medium text-lg">{item.title}</h2>
-                                <p className="text-gray-600">{item.issuer}</p>
-                                <div>
+                                <p className="text-gray-600 text-start">{item.issuer}</p>
+                                <div className="flex justify-start space-x-2 mb-4 items-start">
                                     <p>{item.date}</p>
-                                    <span>{item.status}</span>
+                                    <span className="border border-gray-800 bg-gray-900 text-white px-1.5 text-sm rounded-full">{item.status}</span>
                                 </div>
-                                <p>{item.description}</p>
-                                <div>
-                                    <p>Habilidades Validadas:</p>
+                                <p className="text-start text-gray-600">{item.description}</p>
+                                <p className="text-start mt-3 font-medium mb-1">Habilidades Validadas:</p>
+                                <div className="flex flex-wrap gap-2">
                                     {item.skills.map((skill, index) => (
-                                        <div key={index} className="">
-                                            <span className="">{skill}</span>
+                                        <div key={index} className="border border-gray-300 px-1.5 rounded-full">
+                                            <span className="text-xs font-semibold">{skill}</span>
                                         </div>
                                     ))}
                                 </div>
-                                <Link>Verficar</Link>
+                                <div className="flex justify-between mt-4">
+                                    <Link 
+                                        to={item.certificateUrl}
+                                        className="py-2 bg-gray-900 text-white px-3 rounded-lg"
+                                    >Verificar</Link>
+                                </div>
                             </div>
                         </div>
                     ))}
